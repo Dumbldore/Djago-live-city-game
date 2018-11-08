f08eda85f4dc43424ae8a1b18f1b29d26a9255e2
Dumbldore <mbadocha@gmail.com>
2018-11-08 16:00:29 +0100

    Addedd big buildings ant time bonus for buildings

--- blog/views.py
+++ blog/views.py
@@ -1,81 +1,87 @@
 from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
-from users.models import Profile
+from users.models import Patrol
+from users.models import Building
+from users.models import Big_Building
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-import time
-
-
-
-
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+from background_task import background
+from django.utils import timezone
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
 
+@background(schedule=1)
+def hello(xd):
+    patrol = get_object_or_404(Patrol, id=xd)
+    patrol.people += 5
+    patrol.save()
+
+
 def kod(request):
     if request.GET.get('btn'):
-        profil = get_object_or_404(Profile, user=request.user)
+        profil = get_object_or_404(Patrol, user=request.user)
         xd = request.GET.get('inputed_code')
-        if xd == 'polska' or 'usa' or 'germany':
+        if xd == 'polska' or xd == 'usa':
             messages.success(request, f'Prawidlowy kod!!')
             profil.points += 100
-            profil.usedpoints += " , " + xd
+            profil.usedcodes += " , " + xd
         else:
             messages.warning(request, f'Zly kod!!')
 
-        profil.save(update_fields=["points", "usedpoints"])
-        return render(request, 'users/profile.html')
+        profil.save(update_fields=["points", "usedcodes"])
+        return render(request, 'blog/kod.html')
 
     else:
         return render(request, 'blog/kod.html')
 
 
 def home(request):
-    admin = get_object_or_404(Profile, usedpoints="dumbldore")
-    if request.GET.get('dworcowa3'):
-        if admin.dworcowa3 == 1:
-            messages.warning(request, f'Budynek juz kupiony')
-            return render(request, 'blog/home.html')
-        profil = get_object_or_404(Profile, user=request.user)
-        messages.success(request, f'Kupiles budynek')
-        admin.dworcowa3 = 1
-        profil.dworcowa3 = 1
-        profil.save()
-        admin.save()
-        return render(request, 'blog/home.html')
+    context = {
+        'buildings': Building.objects.all(),
+        'bigbuildings': Big_Building.objects.all(),
+        'posts': Post.objects.all(),
+        'patrol': get_object_or_404(Patrol, user=request.user)
+    }
+    for each in Building.objects.all():
+        building = each
+        if request.GET.get(building.name):
+            if building.built == 1:
+                messages.warning(request, f'Budynek juz zbudowany')
+                return render(request, 'blog/home.html')
+            else:
+                profil = get_object_or_404(Patrol, user=request.user)
+                messages.success(request, f'Brawo!, zbudowales budynek')
+                building.built = 1
+                building.patrol = profil
+                profil.built_buildings += building.name
+                profil.number_of_built_buildings += 1
+                building.date=timezone.now()
+                profil.save()
+                building.save()
+                hello(profil.id, repeat=30)
+                return render(request, 'blog/home.html', context)
 
-    if request.GET.get('dworcowa10'):
-        if admin.dworcowa10 == 1:
-            messages.warning(request, f'Budynek juz kupiony')
-            return render(request, 'blog/home.html')
-        profil = get_object_or_404(Profile, user=request.user)
-        messages.success(request, f'Kupiles budynek')
-        admin.dworcowa10 = 1
-        profil.dworcowa10 = 1
-        profil.save()
-        admin.save()
-        return render(request, 'blog/home.html')
+    for each in Big_Building.objects.all():
+        bigbuilding = each
+        if request.GET.get(bigbuilding.name):
+            if bigbuilding.how_much_built == bigbuilding.size:
+                messages.warning(request, f'Budynek juz zbudowany')
+                return render(request, 'blog/home.html')
+            else:
+                profil = get_object_or_404(Patrol, user=request.user)
+                messages.success(request, f'Brawo!, Dzieki Tobie odbudujemy Gdansk!')
+                bigbuilding.how_much_built += 1
+                bigbuilding.patrol += profil.user.username + ' , '
+                profil.built_buildings += building.name
+                profil.number_of_built_buildings += 1
+                profil.save()
+                bigbuilding.save()
+                return render(request, 'blog/home.html', context)
 
-    if request.GET.get('dworcowa13'):
-        if admin.dworcowa13 == 1:
-            messages.warning(request, f'Budynek juz kupiony')
-            return render(request, 'blog/home.html')
-        profil = get_object_or_404(Profile, user=request.user)
-        messages.success(request, f'Kupiles budynek')
-        admin.dworcowa13 = 1
-        profil.dworcowa13 = 1
-        profil.save()
-        admin.save()
-        return render(request, 'blog/home.html')
-
-    else:
-        return render(request, 'blog/home.html', {'admin': admin})
+        else:
+            return render(request, 'blog/home.html', context)
 # Create your views here.
--- users/models.py
+++ users/models.py
@@ -2,15 +2,41 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Profile(models.Model):
+
+class Patrol(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to="profile_pics")
     points = models.PositiveIntegerField(default=0)
     people = models.PositiveIntegerField(default=0)
-    dworcowa3 = models.BooleanField(default=0)
-    dworcowa10 = models.BooleanField(default=0)
-    dworcowa13 = models.BooleanField(default=0)
-    usedpoints = models.TextField(default='xdddddd')
+    usedcodes = models.TextField(default='xdddddd')
+    id = models.AutoField(primary_key=True)
+    built_buildings = models.TextField(default='xdddddd')
+    number_of_built_buildings = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.username} Profile {self.id}'
+
+
+class Building(models.Model):
+    name = models.TextField()
+    built = models.BooleanField(default=False)
+    image = models.URLField()
+    patrol = models.ForeignKey(Patrol, on_delete=models.CASCADE, blank=True, null=True)
+    id = models.AutoField(primary_key=True)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.name} Building {self.id}'
+
+
+class Big_Building(models.Model):
+    name = models.TextField()
+    how_much_built = models.PositiveIntegerField(default=0)
+    size = models.PositiveIntegerField(default=4)
+    image = models.URLField(default='https://ebilet-media.azureedge.net/media/18709/teatr-dom-git450.jpg', blank=True, null=True)
+    patrol = models.TextField(default='!' , blank=True, null=True)
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return f'{self.name} Big_Building {self.id}'
+
