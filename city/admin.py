c1b98337961ff8d1c2e3c49c526fd3e2f547bcc6
Norbert Szulc <norbert@not7cd.net>
2018-11-09 22:05:32 +0100

    Sortable admin

--- city/admin.py
+++ city/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-from .models import Patrol2, Building, Share, BonusCode, UsedBonusCode
+from .models import Patrol2 as Patrol, Building, Share, BonusCode, UsedBonusCode
 
 # Register your models here.
 
-admin.site.register(Patrol2)
+@admin.register(UsedBonusCode)
+class UsedBonusCodeAdmin(admin.ModelAdmin):
+    list_display = ('bonus_code', 'patrol', 'datetime_used')
+
+
+@admin.register(Patrol)
+class PatrolAdmin(admin.ModelAdmin):
+    list_display = ("name", "money", "people")
+
+
+@admin.register(Share)
+class ShareAdmin(admin.ModelAdmin):
+    list_display = ("building", "patrol", "date_bought")
+
+# admin.site.register(Share)
 admin.site.register(Building)
-admin.site.register(Share)
 admin.site.register(BonusCode)
-admin.site.register(UsedBonusCode)
+
